7d9b28dc55b839e503cf4cf7f75426b14d2b67b4
raphaelcozzi <raphaelcozzi@gmail.com>
2021-01-11 09:35:11 -0200

    Implementado layout static

--- inc/base_functions.php
+++ inc/base_functions.php
@@ -1101,4 +1101,35 @@ function make_combo($query , $name_field , $id_field , $selected_id, $first_opti
     return $combo;
 }
 
+function createZip($nomearquivo = "", $urls = array())
+{
+         # create new zip object
+         $zip = new ZipArchive();
+         $files = $urls;
+
+         # create a temp file & open it
+         $tmp_file = tempnam('.', '');
+         $zip->open($tmp_file, ZipArchive::CREATE);
+
+         # loop through each file
+         foreach ($files as $file) {
+             # download file
+             $download_file = file_get_contents($file);
+
+             #add it to the zip
+             $zip->addFromString(basename($file), $download_file);
+         }
+
+         # close zip
+         $zip->close();
+
+         # send the file to the browser as a download
+         header('Content-disposition: attachment; filename="'.$nomearquivo.'.zip"');
+         header('Content-type: application/zip');
+         readfile($tmp_file);
+         unlink($tmp_file);
+   
+}
+
+
 ?>
